bf3d8f1a39d1fde7141c506ad26bc1a8f14a2fe5
Leonel Donati <leonelroman321@gmail.com>
2024-05-30 16:36:45 -0300

    fixed some styles

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const serverHost: string = import.meta.env.VITE_SERVER_HOST
+export const serverHost: string = import.meta.env.VITE_SERVER_HOST ?? 'http://localhost:8080'
 export const apiVersion: string = 'v1'
 export const defaultAvatar = 'https://res.cloudinary.com/dzmuriaby/image/upload/v1701369252/avatares/ucqpxvyuji2z0gqwbwg9.png'
 
